d0ca05ee4a9fadd24d098739fca91616d47b13c6
nunjayi <epenyash@gmail.com>
2024-04-30 14:26:46 +0300

    hectic list and keys assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,10 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+      <nav>{/* display an <a> tag for each link here */}
+
+      </nav>)
 }
 
 export default NavBar;
